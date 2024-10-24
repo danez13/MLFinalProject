9b917ea04ff5bdaeec334fa85318c4778d0997c6
danez13 <hernandezdaniel170@gmail.com>
2024-10-24 17:57:49 -0400

    UPDATE Train_SVM

--- Model_Training/train_SVM.py
+++ Model_Training/train_SVM.py
@@ -7,12 +7,13 @@ from sklearn.metrics import accuracy_score
 train_df = pd.read_csv("Processed_Data/train_FD001.csv")
 test_df = pd.read_csv("Processed_Data/test_FD001.csv")
 
+# train with sensors
 X_train = train_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
 y_train = train_df[["failure_within_w1"]].values
 
+# test with sensors
 X_test = test_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
 y_test = test_df[["failure_within_w1"]].values
-kernels = ['linear','poly','rbf','sigmoid','precomputed']
 
 model = SVC(C=1,kernel="poly")
 model.fit(X_train, np.ravel(y_train))

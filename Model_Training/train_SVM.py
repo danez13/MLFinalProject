0ce03e7582033e0979dda850b607d2226103c8b9
danez13 <hernandezdaniel170@gmail.com>
2024-11-04 09:31:43 -0500

    UPDATE train_SVM

--- Model_Training/train_SVM.py
+++ Model_Training/train_SVM.py
@@ -2,30 +2,28 @@ from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 
-train_df = pd.read_csv("Processed_Data/train_FD001.csv")
-test_df = pd.read_csv("Processed_Data/test_FD001.csv")
+for i in range (1,5):
+    train_df = pd.read_csv(f"Processed_Data/train_FD00{i}.csv")
+    test_df = pd.read_csv(f"Processed_Data/test_FD00{i}.csv")
 
-# train with sensors
-X_train = train_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
-y_train = train_df[["failure_within_w1"]].values
+    # train with sensors
+    X_train = train_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
+    y_train = train_df[["failure_within_w1"]].values
 
-# test with sensors
-X_test = test_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
-y_test = test_df[["failure_within_w1"]].values
+    # test with sensors
+    X_test = test_df[["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8", "s_9", "s_10", "s_11", "s_12", "s_13", "s_14", "s_15", "s_16","s_17","s_18","s_19","s_20","s_21"]].values
+    y_test = test_df[["failure_within_w1"]].values
 
-model = SVC(C=1,kernel="poly")
-model.fit(X_train, np.ravel(y_train))
+    model = SVC(C=1,kernel="poly")
+    model.fit(X_train, np.ravel(y_train))
 
-print(f"Model weight w0: {model.intercept_}")
+    y_pred = model.predict(X_test)
 
-print(f"Model weight w1 and w2: {model.coef0}")#type: ignore
-
-print(f"confidence scores: {model.decision_function(X_train)}")
-
-y_pred = model.predict(X_test)
-
-score = model.score(X_test,np.ravel(y_test))
-print(f"accuracy score: {score}")
+    score = model.score(X_test,np.ravel(y_test))
+    cm = confusion_matrix(y_test,y_pred)
+    cm_display = ConfusionMatrixDisplay(cm)
+    cm_display.plot()
+    print(score)
 
